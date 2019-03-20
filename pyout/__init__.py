a4fe66c94e8bcc35194049fd1f5f5b6deafcaab6
Kyle Meyer <kyle@kyleam.com>
2019-03-20 16:10:42 -0400

    Define a "no update, no styling" replacement for Tabular
    We don't support Windows.  In order to do so, we'd need to (1) define
    an interface that doesn't use blessings, (2) add support for rewriting
    previous lines, and (3) add support for styling output.  We could at
    least get some of the way by directly writing ANSI codes and using
    colorama.  But that is for another time, and probably for someone else
    who cares about supporting Windows.
    
    Instead, do no. 1 by adding a mostly useless replacement for Tabular
    that doesn't support updating or styling, with the unambitious goal of
    having something that doesn't crash on Windows.
    
    Re: #4

--- pyout/__init__.py
+++ pyout/__init__.py
@@ -5,7 +5,15 @@ Exposes a single entry point, the Tabular class.
 
 from __future__ import unicode_literals
 
+import sys
+
 __version__ = "0.1.0"
 
 from pyout.elements import schema
-from pyout.tabular import Tabular
+
+if sys.platform == "win32":
+    from pyout.tabular_dummy import Tabular
+else:
+    from pyout.tabular import Tabular
+
+del sys
---
+++ pyout/tabular_dummy.py
@@ -0,0 +1,64 @@
+"""Not much of an interface for styling tabular terminal output.
+
+This module defines a mostly useless Tabular entry point.  Previous lines are
+not updated, and the results are not styled (e.g., no coloring or bolding of
+values).  In other words, this is not a real attempt to support Windows.
+"""
+
+from __future__ import unicode_literals
+
+import sys
+
+from pyout import interface
+from pyout.common import ContentWithSummary
+from pyout.common import StyleFields
+from pyout.field import StyleProcessors
+
+
+class NoUpdateTerminalStream(interface.Stream):
+
+    supports_updates = False
+
+    def __init__(self, stream=None):
+        self.stream = stream or sys.stdout
+
+    def _die(self, *args, **kwargs):
+        raise NotImplementedError("{!s} does not support 'update' methods"
+                                  .format(self.__class__.__name__))
+
+    clear_last_lines = _die
+    overwrite_line = _die
+    move_to = _die
+
+    # Height and width are the fallback defaults of py3's
+    # shutil.get_terminal_size().
+
+    @property
+    def width(self):
+        return 80
+
+    @property
+    def height(self):
+        return 24
+
+    def write(self, text):
+        self.stream.write(text)
+
+
+class NoopProcessors(StyleProcessors):
+
+    def render(self, _, value):
+        return value
+
+
+class Tabular(interface.Writer):
+    """Like `pyout.tabular.Tabular`, but broken.
+
+    This doesn't support terminal styling or updating previous content.
+    """
+
+    def __init__(self, columns=None, style=None):
+        self._stream = NoUpdateTerminalStream()
+        self._content = ContentWithSummary(
+            StyleFields(style, NoopProcessors()))
+        super(Tabular, self).__init__(columns, style)
