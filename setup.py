3569e5be03dd436c4d063bde5bad81ae0cf042b0
Kyle Meyer <kyle@kyleam.com>
2019-10-21 13:45:30 -0400

    setup.py: Remove py2-related bits

--- setup.py
+++ setup.py
@@ -1,8 +1,7 @@
 from setuptools import setup
 
 requires = {
-    "core": ["blessings; sys_platform != 'win32'",
-             "six"],
+    "core": ["blessings; sys_platform != 'win32'"],
     "tests": ["pytest", "pytest-timeout", "mock"],
     "validation": ["jsonschema"],
 }
@@ -30,8 +29,6 @@ setup(
         "Development Status :: 1 - Planning",
         "Topic :: Utilities",
         "Operating System :: POSIX",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Topic :: Software Development :: Libraries",
